b30e44df5b77cb5f9df04f80ccbfba6a385fd85c
kkrachenfels <kkrachenfels@ucdavis.edu>
2022-01-08 13:02:14 -0800

    Changed system() to fork() + exec() + wait()
    -not tested thoroughly but the basic idea is there (for ex, the date command works)
    -still need to do for phase 1 (copied from the project doc):
    -The completion message following the execution of the command is printed to stderr and not stdout.
    The printed status (i.e. 0 in the example above) is not the full raw status value anymore, it is the exit status only. Refer to the Process Completion Status section of the libc documentation to understand how to extract this value

--- sshell.c
+++ sshell.c
@@ -38,9 +38,36 @@ int main(void)
                 }
 
                 /* Regular command */
-                retval = system(cmd);
+                /*retval = system(cmd);
                 fprintf(stdout, "Return status value for '%s': %d\n",
-                        cmd, retval);
+                        cmd, retval); */
+
+
+                //replacing code above:
+
+                pid_t pid; //declare pid
+                //no real args for now (replace second NULL in phase 2):
+                char *args[] = {cmd, NULL, NULL};
+
+                pid = fork(); //fork off new process
+                if (pid == 0) {
+                        //child
+                        execvp(cmd, args); //use execvp (the -p specifies to use $PATH)
+                        perror("error in execv"); //if the exec doesn't work
+                        exit(1);
+                } 
+                else if (pid > 0) {
+                        //parent
+                        int status;
+                        waitpid(pid, &status, 0); //wait for child process to finish exec
+                        printf("Return status value for '%s': %d\n",
+                                cmd, WEXITSTATUS(status));
+                }
+                else {
+                        perror("forking error");
+                        exit(1);
+                }
+
         }
 
         return EXIT_SUCCESS;
