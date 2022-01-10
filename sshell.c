126c0a15de9a10a5ab0a60ad3cbfe1628b9dcd2f
aliyaabbas <aliyaabbas11@gmail.com>
2022-01-09 19:17:40 -0800

    phase 2 & 3
    1) command object structure
    2) parsing and tokenizing command line
    3) detecting redirection filename

--- sshell.c
+++ sshell.c
@@ -5,6 +5,91 @@
 
 #define CMDLINE_MAX 512
 
+// struct to represent a command.
+struct myCmdObj {
+    char cmdName[32];      // cmd name i.e. ls
+    char *cmdOptions[16];  // cmd options i.e.  ls -l
+    int cmdOptionsCount;
+    char redirectionFileName[32];  // redirection file name.
+    int status;               
+};
+
+// maximum of 16 commands.
+struct myCmdObj commandsObj[16] = {0};
+
+// Reset and free memory for commandObj struct.
+void ResetCommandObj()
+{
+        for(int i=0; i<16; ++i) {
+                for(int j=0; j<=commandsObj[i].cmdOptionsCount; ++j) 
+                {
+                        if(commandsObj[i].cmdOptions[j] != NULL){
+                                free(commandsObj[i].cmdOptions[j]);
+                        }
+                        commandsObj[i].cmdOptions[j] = NULL;
+                }
+
+                commandsObj[i].cmdOptionsCount = 0;
+                commandsObj[i].cmdName[0] = '\0';      
+        }
+}
+
+// Phase 2, parsing and tokenizing the command line and the options.
+void ParseCommandLine(char *cmd) 
+{
+        int isFirstToken = 1, isRedirection = 0;
+        int cmdCounter = 0, cmdOptionCounter = 0;
+        char *token = strtok (cmd," ");
+
+        while (token != NULL)
+        {
+                printf ("PRINTING current token: %s\n",token);
+                if(isFirstToken){
+                        // first token is the command name.
+                        strcpy(commandsObj[cmdCounter].cmdName,token);
+                        commandsObj[cmdCounter].cmdOptions[cmdOptionCounter] = (char *) malloc(32 * sizeof(char));
+                        strcpy(commandsObj[cmdCounter].cmdOptions[cmdOptionCounter],token);
+                        isFirstToken = 0;
+                        cmdOptionCounter++;
+                } else {
+                        // not first token, then command option.
+                        if (!strcmp(token, ">")) {
+                                isRedirection = 1;
+                                printf("  Got Redirection\n");
+                        } else if(isRedirection) {
+                                //save redirection filename.
+                                strcpy(commandsObj[cmdCounter].redirectionFileName,token);
+                                isRedirection = 0;
+                        }
+                        else {
+                                commandsObj[cmdCounter].cmdOptions[cmdOptionCounter] = (char *) malloc(32 * sizeof(char));
+                                strcpy(commandsObj[cmdCounter].cmdOptions[cmdOptionCounter],token);
+                                cmdOptionCounter++;
+                        }
+                }
+
+                if (!strcmp(token, "|")) {
+                        cmdCounter++;
+                        printf("  Got Pipe: TODO.\n");
+                        break;      
+                } 
+
+                token = strtok(NULL, " ");
+        }
+        // save the number of options for this command
+        commandsObj[cmdCounter].cmdOptionsCount = cmdOptionCounter;
+}
+
+// Phase 3, Implement Built-in commands cd in a shell. 
+void CmdInShellCd(char* cmd)
+{
+        char *cdToken = strtok(cmd," ");
+        char *dirNameToken = strtok (NULL, " ");
+
+        int returnValue = chdir(dirNameToken);
+        fprintf(stderr, "+ completed '%s' [%d]\n", cmd, returnValue);
+}
+
 int main(void)
 {
         char cmd[CMDLINE_MAX];
@@ -35,24 +120,28 @@ int main(void)
                 if (!strcmp(cmd, "exit")) {
                         fprintf(stderr, "Bye...\n");
                         break;
+                } else if (!strcmp(cmd, "pwd")) {
+                        char *cwd = getcwd(NULL,0);
+                        fprintf(stdout, "%s\n",cwd);
+                        fprintf(stderr, "+ completed '%s' [%d]\n", cmd, 0);
+                        continue;
+                } else if (cmd[0] == 'c' && cmd[1] == 'd' && cmd[2] == ' ' ) {
+                        CmdInShellCd(cmd);
+                        continue;
                 }
 
-                /* Regular command */
-                /*retval = system(cmd);
-                fprintf(stdout, "Return status value for '%s': %d\n",
-                        cmd, retval); */
-
-
-                //replacing code above:
+                ParseCommandLine(cmd);
 
                 pid_t pid; //declare pid
                 //no real args for now (replace second NULL in phase 2):
-                char *args[] = {cmd, NULL, NULL};
-
+                if(commandsObj[0].cmdName[0] == '\0'){
+                        continue;
+                }
                 pid = fork(); //fork off new process
                 if (pid == 0) {
                         //child
-                        execvp(cmd, args); //use execvp (the -p specifies to use $PATH)
+                        //execvp(cmd, args);
+                        execvp(commandsObj[0].cmdName, commandsObj[0].cmdOptions); //use execvp (the -p specifies to use $PATH)
                         perror("error in execv"); //if the exec doesn't work
                         exit(1);
                 } 
@@ -67,6 +156,9 @@ int main(void)
                         perror("forking error");
                         exit(1);
                 }
+
+                // Reset command object struct.
+                ResetCommandObj();
 
         }
 
